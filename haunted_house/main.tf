a170fd5b4400a83c4216eeb4aa8f65f784454347
Jaap de Koning <jaap.de.koning@bigdatarepublic.nl>
2021-10-16 17:11:59 +0200

    chore: fix formatting

--- haunted_house/main.tf
+++ haunted_house/main.tf
@@ -1,6 +1,6 @@
 # An example resource that does nothing.
 resource "null_resource" "example" {
- triggers = {
-   value = "A example resource that does nothing!"
- }
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
 }
